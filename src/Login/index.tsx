8323639d1f47430c41ea28648827cb6c43b8d32e
marceloabbadia <marcelo.abbadia@gmail.com>
2023-06-16 18:27:45 -0300

    adicionado os primeiros codigos da tela de app, para o primeiro trabalho do modulo

---
+++ src/Login/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+import MeuInput from "../../components/MeuInput";
+
+export default function Login() {
+  return (
+    <View>
+      <Text>Login</Text>\<MeuInput></MeuInput>
+      <MeuInput></MeuInput>
+      <Text>Esqueci minha senha</Text>
+      <Text>Entrar</Text>
+    </View>
+  );
+}
